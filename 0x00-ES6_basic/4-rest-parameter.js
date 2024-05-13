854d82d72d15cc95c455a5453894d36ec9effbf2
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-13 03:06:49 -0700

    count

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...numbers) {
-  let count = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    count += 1;
-  }
-  return count;
+  return numbers.length;
 }
