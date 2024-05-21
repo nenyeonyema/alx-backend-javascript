15dabe085c390777ab98f2e8564f96b384aa8fd7
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-21 05:25:18 -0700

    getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,55 @@
+// Implements a class named HolbertonCourse
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+    // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(value) {
+    this._name = this._validateName(value);
+  }
+
+  set length(value) {
+    this._length = this._validateLength(value);
+  }
+
+  set students(value) {
+    this._students = this._validateStudents(value);
+  }
+
+  // Validation methods
+  _validateName(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    return name;
+  }
+
+  _validateLength(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    return length;
+  }
+
+  _validateStudents(students) {
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    return students;
+  }
+}
