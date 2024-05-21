76dcd041165fae14bc3309837240dc873be72d06
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-21 04:56:31 -0700

    modified

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,7 @@
-// Implements a function named initializeRooms
 import ClassRoom from './0-classroom.js';
 
 
 export default function initializeRooms() {
-  // const roomSizes = [19, 20, 34];
-  //return roomSizes.map(size => new ClassRoom(size));
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-  return [room1, room2, room3];
+  const roomSizes = [19, 20, 34];
+  return roomSizes.map(size => new ClassRoom(size));
 }
