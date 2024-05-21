48576d7f1920fce23283503a1d3ebd4c398fa5a7
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-21 04:40:06 -0700

    array of classroom objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,7 @@
 // Implements a function named initializeRooms
 import ClassRoom from './0-classroom.js';
 
+
 export default function initializeRooms() {
   const roomSizes = [19, 20, 34];
   return roomSizes.map(size => new ClassRoom(size));
