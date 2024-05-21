515189f2002ad367e70550172908c34c14ee9628
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-21 04:47:09 -0700

    array of classroom objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,6 +3,10 @@ import ClassRoom from './0-classroom.js';
 
 
 export default function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  return roomSizes.map(size => new ClassRoom(size));
+  // const roomSizes = [19, 20, 34];
+  //return roomSizes.map(size => new ClassRoom(size));
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  return [room1, room2, room3];
 }
