33a823f0a33d4a80aade656a405d05e45beb0a83
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-02 11:57:23 -0700

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,27 +1,12 @@
-// 5-building.js
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly');
+    this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = this._validateNumber(sqft, 'Square feet');
   }
 
-  // Getter
   get sqft() {
     return this._sqft;
-  }
-
-  // Validation method
-  _validateNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
-
-  // Method to ensure implementation in subclass
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
