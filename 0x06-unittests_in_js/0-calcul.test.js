cf3ea5ccfde89e9a5ed07581fd255b3f8cf49e57
nenyeonyema <chinenyeonyema1@gmail.com>
2024-07-19 02:29:42 -0700

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,31 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', function () {
-  it('should return 4 when inputs are 1 and 3', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
+describe("calculateNumber()", function() {
 
-  it('should return 5 when inputs are 1 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when inputs are 1.2 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when inputs are 1.5 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should return 0 when inputs are 0.4 and -0.4', function () {
-    assert.strictEqual(calculateNumber(0.4, -0.4), 0);
-  });
-
-  it('should handle negative numbers properly', function () {
-    assert.strictEqual(calculateNumber(-1.5, -3.5), -4);
-  });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
+    });
 });
